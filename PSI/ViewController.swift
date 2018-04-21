ddb2f7ed163e72018b8b598ad0ca0ce13ad7b1e9
Jacky Tjoa <jacky.coolheart@gmail.com>
2018-04-21 20:16:37 +0800

    Add MapView to UI and connect outlet

--- PSI/ViewController.swift
+++ PSI/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
