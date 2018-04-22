7369c3278f2b16dcb4c7c92986cb792aee86fc84
Jacky Tjoa <jacky.coolheart@gmail.com>
2018-04-22 13:18:48 +0800

    Setup QuickSpec

--- PSITests/PSITests.swift
+++ PSITests/PSITests.swift
@@ -6,31 +6,13 @@
 //  Copyright © 2018 Jacky Tjoa. All rights reserved.
 //
 
-import XCTest
+import Quick
+import Nimble
 @testable import PSI
 
-class PSITests: XCTestCase {
+class PSITests: QuickSpec {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func spec() {
+       
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
